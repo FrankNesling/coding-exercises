ed96d238aa3eaa094a60326ce28eb3e2cede816f
franknesling <matthias.gnilsen@hotmail.com>
2026-03-03 20:24:19 +0100

    with memoization

--- Python/memoization.py
+++ Python/memoization.py
@@ -9,6 +9,19 @@ def step_count(n, stepsizes):
 
   return sum(step_count(n-s, stepsizes) for s in stepsizes)
 
+def mem_count(n, stepsizes, cache = {}):
+  if n == 0:  # valid step combination
+    return 1
+  elif n < 0: # invalid step combination
+    return 0
+
+  # add element to cache if not yet calculated
+  if n not in cache:
+    cache[n] = sum(mem_count(n-s, stepsizes, cache) for s in stepsizes)
+
+  return cache[n]
+
+
 stepcount = int(input("How many stepsizes?\n"))
 stepsizes = []
 
@@ -20,5 +33,11 @@ n = int(input("How many steps?\n"))
 
 start = time.time()
 print(step_count(n, stepsizes))
+end = time.time()
+print(f'Elapsed time: {end - start}')
+
+print("With memoization: ")
+start = time.time()
+print(mem_count(n, stepsizes))
 end = time.time()
 print(f'Elapsed time: {end - start}')
